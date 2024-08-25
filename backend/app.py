0ae51fa8ae7e53c72f13868197a33654563a2cc2
Niladitya Sen <niladityasen.2212@gmail.com>
2024-08-25 11:25:47 +0530

    first commit created a simple pdf redactor

---
+++ backend/app.py
@@ -0,0 +1,48 @@
+from pydoc import doc
+import pytesseract
+import os
+from pdf2image import convert_from_path
+import spacy
+import codecs
+
+nlp = spacy.load("en_core_web_trf")
+
+pdf_path = f"{os.path.dirname(__file__)}/pdf/test.pdf"
+
+images = convert_from_path(pdf_path)
+
+text = ""
+
+for page_number, page in enumerate(images):
+    # Convert the page to grayscale
+    gray_image = page.convert("L")
+
+    # Use Tesseract to extract text from the image
+    text += pytesseract.image_to_string(gray_image)
+
+
+doc = nlp(text)
+
+redacted_text = text
+
+for entity in doc.ents:
+    if entity.label_ in [
+        "PERSON",
+        "ORG",
+        "GPE",
+        "DATE",
+        "TIME",
+        "MONEY",
+        "QUANTITY",
+        "ORDINAL",
+        "CARDINAL",
+        "ADDRESS",
+        "PHONE_NUMBER",
+        "EMAIL",
+        "ACCOUNT_NUMBER",
+    ]:
+        redacted_text = redacted_text.replace(entity.text, "█" * len(entity.text))
+
+with codecs.open(f"{os.path.dirname(__file__)}/output.txt", "w", encoding="utf-8") as file:
+    file.write(redacted_text)
+
